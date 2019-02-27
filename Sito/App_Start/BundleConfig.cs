8440e39f08b58eb683b44562b2befb54c766dfae
Danefriser3 <parisi_daniele@hotmail.it>
2019-02-27 22:16:21 +0100

    Added Cookie to not flood igdb api.

--- Sito/App_Start/BundleConfig.cs
+++ Sito/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace Sito
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.cookie.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/underscore").Include(
                         "~/Scripts/underscore.js"));
